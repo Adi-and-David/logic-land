c9cf62c72add22628e96c8a3e01bc054887152ad
aditya7iyengar <adi.iyengar@annkissam.com>
2016-02-07 16:15:15 -0500

    Remove global url config

--- logic_land/urls.py
+++ logic_land/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('logic_world.urls')),
+    url(r'^', include('logic_world.urls')),
     url(r'^admin/', admin.site.urls),
 
 ]
