45164afac4f2abc6ccddd1cb01c666ec911f5624
aditya7iyengar <adi.iyengar@annkissam.com>
2016-02-07 18:31:53 -0500

    Add interface to admin page to edit models

--- logic_world/admin.py
+++ logic_world/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
+from .models import World
+from .models import UserWorldRelation
+
+admin.site.register(User)
+admin.site.register(World)
+admin.site.register(UserWorldRelation)
 
 # Register your models here.
