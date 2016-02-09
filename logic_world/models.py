b234c79539315153d9d376d2e90b704783084c39
dat1010 <tannerd@mail.gvsu.edu>
2016-02-09 18:43:23 -0500

    Initial commit
    Commit to make pull request

--- logic_world/models.py
+++ logic_world/models.py
@@ -7,11 +7,13 @@ from django.db import models
 class World(models.Model):
 	user_count = models.IntegerField()
 
-class User(models.Model):
+''' Test by commenting out
+	Will be removed after testing.
+class User(modelsdModel):
 	email = models.CharField(max_length=200)
 	user_name = models.CharField(max_length=200)
-	world = models.ForeignKey(World)
-
+	world = models.ForeignKey(dWorld)
+'''
 class UserWorldRelation(models.Model):
 	user = models.ForeignKey(User)
 	world =  models.ForeignKey(World)
