2fb1e0e6e1aceb72f6389fcc03ead3ffe16b1603
aditya7iyengar <adi.iyengar@annkissam.com>
2016-02-12 17:56:53 -0500

    Remove users

--- logic_world/models.py
+++ logic_world/models.py
@@ -7,13 +7,6 @@ from django.db import models
 class World(models.Model):
 	user_count = models.IntegerField()
 
-''' Test by commenting out
-	Will be removed after testing.
-class User(modelsdModel):
-	email = models.CharField(max_length=200)
-	user_name = models.CharField(max_length=200)
-	world = models.ForeignKey(dWorld)
-'''
 class UserWorldRelation(models.Model):
 	world =  models.ForeignKey(World)
 	relationship_type = models.CharField(max_length=200)
