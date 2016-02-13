46728ed9732e659ab1e18a704188d2e1e0cd0a19
aditya7iyengar <adi.iyengar@annkissam.com>
2016-02-12 21:10:05 -0500

    Add default; Remove null constraint

--- logic_world/models.py
+++ logic_world/models.py
@@ -1,16 +1,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.contrib.auth.models import User as DjangoUser
 
 # Create your models here.
 
 class World(models.Model):
-	user_count = models.IntegerField()
+	user_count = models.IntegerField(default=0, blank=True, null=True)
 
 # User table and World table should have many-to-many relationship.
-class User(DjangoUser):
-  world_count = models.IntegerField(default=None, blank=True, null=True)
+class User(models.Model):
+  world_count = models.IntegerField(default=0, blank=True, null=True)
 
 class UserWorldRelation(models.Model):
 	world =  models.ForeignKey(World)
