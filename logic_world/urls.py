8706855fe385c524aa4306725380e0875ced5346
aditya7iyengar <adi.iyengar@annkissam.com>
2016-02-15 15:23:07 -0500

    Add url for create

--- logic_world/urls.py
+++ logic_world/urls.py
@@ -4,9 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-
     url(r'^(?P<world_id>[0-9]+)/$', views.show, name='show'),
-
-    #TODO learn how to post action for create
-    #url(r'^$', views.create, name='create'),
+    url(r'^/new$', views.create, name='new')
 ]
