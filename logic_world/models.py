a45a5072f8208ad81b4de74c2c383996a5d995e4
aditya7iyengar <adi.iyengar@annkissam.com>
2016-02-15 14:09:19 -0500

    Add world element model

--- logic_world/models.py
+++ logic_world/models.py
@@ -15,6 +15,13 @@ class World(models.Model):
   user_count = models.IntegerField(default=0, blank=True, null=True)
 
 class UserWorldRelation(models.Model):
-  world =  models.ForeignKey(World, on_delete=models.CASCADE, null=True)
+  world = models.ForeignKey(World, on_delete=models.CASCADE, null=True)
   custom_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True)
   relationship_type = models.CharField(max_length=200)
+
+class WorldElement(models.Model):
+  creator_world_relation = models.ForeignKey(UserWorldRelation, on_delete=models.CASCADE, null=True)
+  x = models.IntegerField(default=0, blank=True, null=True)
+  y = models.IntegerField(default=0, blank=True, null=True)
+  z = models.IntegerField(default=0, blank=True, null=True)
+
