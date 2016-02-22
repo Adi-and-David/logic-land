24f1c16727076c6b688c0d22fae1e41656ace275
aditya7iyengar <adi.iyengar@annkissam.com>
2016-02-22 18:10:53 -0500

    Update urls path

--- logic_world/urls.py
+++ logic_world/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<world_id>[0-9]+)/$', views.show, name='show'),
-    url(r'^/new$', views.create, name='new')
+    url(r'^new$', views.create, name='new')
 ]
